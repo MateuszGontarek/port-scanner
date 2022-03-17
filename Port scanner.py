31e5a328a3e86188ae06987257a9cf273f0a2bfc
MateuszGontarek <92920318+MateuszGontarek@users.noreply.github.com>
2022-03-17 14:33:46 +0100

    Add port range
    You can now specify port range

--- Port scanner.py
+++ Port scanner.py
@@ -1,11 +1,19 @@
 import socket
+def scan(url, portStart = 0, portEnd = 100):
+    for i in range(portStart, portEnd):
+        location = (url, i)
+        if a_socket.connect_ex(location) == 0: print(i, "Port is open")
+        else: print(i, "Port is not open")
+        a_socket.close()
+
 a_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 a_socket.settimeout(1)
 
-url = "www.google.com"
-for i in range(100):
-    location = (url, i)
-    if a_socket.connect_ex(location) == 0: print(i, "Port is open")
-    else: print(i, "Port is not open")
-    a_socket.close()
+url = input('Specify url: ')
+wants = input('Do you want to specify port range: ')
 
+if wants == 'yes':
+    portStart = int(input('The port you want to start: '))
+    portEnd = int(input('The port you want to end: '))
+    scan(url, portStart, portEnd)
+else: scan(url)
