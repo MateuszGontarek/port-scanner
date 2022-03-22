a8505506ad7894ed6921ad7ac7a404095dde192b
MateuszGontarek <92920318+MateuszGontarek@users.noreply.github.com>
2022-03-22 09:25:33 +0100

    Some small changes

--- Port scanner.py
+++ Port scanner.py
@@ -1,13 +1,11 @@
 import socket
-def scan(url, portStart = 0, portEnd = 100):
-    for i in range(portStart, portEnd):
-        location = (url, i)
-        if a_socket.connect_ex(location) == 0: print(i, "Port is open")
+def scan(url, PortStart = 0, PortEnd = 100):
+    for i in range(PortStart, PortEnd):
+        a_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        a_socket.settimeout(1)
+        if a_socket.connect_ex((url, i)) == 0: print(i, "Port is open")
         else: print(i, "Port is not open")
         a_socket.close()
-
-a_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-a_socket.settimeout(1)
 
 url = input('Specify url: ')
 wants = input('Do you want to specify port range: ')
@@ -16,4 +14,5 @@ if wants == 'yes':
     portStart = int(input('The port you want to start: '))
     portEnd = int(input('The port you want to end: '))
     scan(url, portStart, portEnd)
-else: scan(url)
+else: 
+    scan(url)
