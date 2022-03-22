01597730bb07ab076a84c8eddb97a79da3ab8561
MateuszGontarek <92920318+MateuszGontarek@users.noreply.github.com>
2022-03-22 09:41:57 +0100

    Add some colors

--- Port scanner.py
+++ Port scanner.py
@@ -1,10 +1,14 @@
 import socket
-def scan(url, PortStart = 0, PortEnd = 100):
+def scan(url = 'www.google.com', PortStart = 0, PortEnd = 100):
     for i in range(PortStart, PortEnd):
         a_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         a_socket.settimeout(1)
-        if a_socket.connect_ex((url, i)) == 0: print(i, "Port is open")
-        else: print(i, "Port is not open")
+        if a_socket.connect_ex((url, i)) == 0: 
+            print("\033[1;32;40m")
+            print(i, " Port is open")
+        else: 
+            print("\033[1;31;40m")
+            print(i, " Port is not open")
         a_socket.close()
 
 url = input('Specify url: ')
